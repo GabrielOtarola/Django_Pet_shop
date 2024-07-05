b013fb0c2c6ac4e9e92624179a13a6de275e9bc8
Gabriel Otárola <ga.otarola@duocuc.cl>
2024-07-05 01:14:22 -0400

    se conecta alimento_seco_gato.html, alimento_humedo_gato.html, alimento_perro.html, accesorios.html, api.html al proyecto

--- mipetshop/urls.py
+++ mipetshop/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, alimento_seco_gato, alimento_humedo_gato, alimento_perro, accesorios, api
 
 urlpatterns = [
     path('', index, name='index'),
+    path('alimento-seco-gato', alimento_seco_gato, name='alimento_seco_gato'),
+    path('alimento-humedo-gato/', alimento_humedo_gato, name='alimento_humedo_gato'),
+    path('alimento-perro/', alimento_perro, name='alimento_perro'),
+    path('accesorios/', accesorios, name='accesorios'),
+    path('api/', api, name='api'),
 ]
--- mipetshop/views.py
+++ mipetshop/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mipetshop/index.html')
+
+def alimento_seco_gato(request):
+    return render(request, 'mipetshop/alimento_seco_gato.html')
+
+def alimento_humedo_gato(request):
+    return render(request, 'mipetshop/alimento_humedo_gato.html')
+
+def alimento_perro(request):
+    return render(request, 'mipetshop/alimento_perro.html')
+
+def accesorios(request):
+    return render(request, 'mipetshop/accesorios.html')
+
+def api(request):
+    return render(request, 'mipetshop/api.html')
+
